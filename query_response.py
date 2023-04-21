9236570132a40adfc2927a0fa3fc6262c6402e2c
horse-3903 <chongchoonhourafael@gmail.com>
2023-04-21 09:50:49 +0800

    patch #1
    fix file directory

--- query_response.py
+++
@@ -1,7 +0,0 @@
-'''
-This is the module imported in the main code.
-Here we generate the response for the user's query about HCI.
-'''
-
-def response(query):
-    return "WIP"
---
+++ src/main.py
@@ -0,0 +1,31 @@
+import nextcord
+from nextcord.ext import commands
+
+import query_response
+
+import json
+
+# create bot
+bot = commands.Bot(command_prefix="!", intents=nextcord.Intents.all())
+
+# QUERY command
+@bot.command(name="query")
+async def query_command(ctx):
+    # get the query
+    query = ctx.message.content[2 + len("query"):]
+
+    # reply
+    await ctx.reply(query_response.response(query))
+
+# login confirmation
+@bot.event
+async def on_ready():
+    print(f"LOGGED IN AS: {bot.user.name}")
+
+if __name__ == "__main__":
+    # load bot data
+    with open("bot.json", "r") as r:
+        bot_data = json.load(r)
+
+    # start bot
+    bot.run(bot_data["token"])
