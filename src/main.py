c10c3d9bf4210431a1b976df707f771f22256a4f
horse-3903 <chongchoonhourafael@gmail.com>
2023-05-12 10:02:22 +0800

    patch #2
    inserted query into gpt-2 model rudimentarily
    added quit command

--- src/main.py
+++ src/main.py
@@ -1,26 +1,41 @@
 import nextcord
 from nextcord.ext import commands
-
+from nextcord.ext.commands.context import Context
 import query_response
-
 import json
+
+import torch
+from transformers import pipeline
+
+generator = pipeline("text-generation", model="gpt2")
+
+# non-essential
+import sys
+import time
 
 # create bot
 bot = commands.Bot(command_prefix="!", intents=nextcord.Intents.all())
 
-# QUERY command
+# !query command
 @bot.command(name="query")
-async def query_command(ctx):
+async def query_command(ctx:Context):
     # get the query
     query = ctx.message.content[2 + len("query"):]
-
     # reply
-    await ctx.reply(query_response.response(query))
+    await ctx.send(query_response.query_response(query, generator=generator))
+
+# non-essential
+@bot.command(name="quit")
+async def quit_command(ctx:Context):
+    global tokenizer
+    await ctx.send(f"Exiting at {time.strftime('%Y-%m-%d %H:%M:%S')}")
+    tokenizer.save_pretrained("./model/")
+    sys.exit()
 
 # login confirmation
 @bot.event
 async def on_ready():
-    print(f"LOGGED IN AS: {bot.user.name}")
+    print(f"Logged in at {time.strftime('%Y-%m-%d %H:%M:%S')}")
 
 if __name__ == "__main__":
     # load bot data
--- src/query_response.py
+++ src/query_response.py
@@ -3,5 +3,11 @@ This is the module imported in the main code.
 Here we generate the response for the user's query about HCI.
 '''
 
-def response(query):
-    return "WIP"
+from transformers import pipeline
+
+def query_response(query, generator:pipeline):
+    responses = [text["generated_text"] for text in generator(query, max_length=60, num_return_sequences=3)]
+    print(responses)
+    responses = [text.replace("\n\n", "\n") for text in responses]
+    return "Responses Given : \n" + "\n\n\n".join(responses)
+
